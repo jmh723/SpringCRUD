ace4f53109efc8a8fc709161ce54febc8a7726f1
CYAN <cyan@kumobako.net>
2023-07-07 19:46:09 +0900

    comment: SpringContoller.java 주석 변경
    12라인 주석 오타 일부 수정

--- src/main/java/com/jmh/springcrud/controller/SpringContoller.java
+++ src/main/java/com/jmh/springcrud/controller/SpringContoller.java
@@ -9,6 +9,6 @@ public class SpringContoller {
     @RequestMapping("/spring")
     public String spring() {
         return "spring";        /* 원시적으로 return값으로 /spring.html를 입력하지만, application.yml 설정 값
-                                  요 suffix를 .html로 했기 때문에 전달받는 템플릿 html명만 리턴값으로 넣어도 된다 */
+                                   suffix를 .html로 했기 때문에 전달받는 템플릿 html명만 리턴값으로 넣어도 된다 */
     }
 }
